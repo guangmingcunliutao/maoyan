39fe4a5b28c67f0e4139a4c97463497c387847df
xiuse-liutao <1018694341@qq.com>
2018-11-27 20:25:34 +0800

    不重要的提交

--- src/common/cinemaInfo/cinemaInfo.vue
+++ src/common/cinemaInfo/cinemaInfo.vue
@@ -8,8 +8,8 @@
             </span>
         </div>
         <div class="location">
-            <span class="">{{cinemaItem.addr}}</span>
-            <span>{{cinemaItem.distance}}</span>
+            <span class="addr">{{cinemaItem.addr}}</span>
+            <span class="distance">{{cinemaItem.distance}}</span>
         </div>
         <div class="lable"></div>
         <div class="discount"></div>
@@ -41,12 +41,34 @@ export default {
         white-space: nowrap;
         .itemnm{
             font-size: .32rem;
+            margin-right: .1rem;
         }
         .price,.q{
+            height: .5rem;
             color: red;
             font-size: .38rem;
         }
+        .q{
+            font-size: .24rem;
+        }
+    }
+}
+.location{
+    height: .4rem;
+    line-height: .4rem;
+    margin-top: .1rem;
+    span{
+        display: inline-block;
+        vertical-align: middle;
+    }
+    .addr{
+        width: 4.3rem;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+    .distance{
+        float: right;
     }
 }
 </style>
-
